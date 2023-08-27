21d1e49da501ac0c3b51ca0af63385881fefcecc
hankshackleford <143135567+hankshackleford@users.noreply.github.com>
2023-08-27 12:37:45 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -6,6 +6,8 @@ Created on Fri Aug 25 15:19:37 2023
 """
 
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title="Korbel Fantasy League",
@@ -17,3 +19,23 @@ st.text('Select a page from the sidebar to get started.')
 
 
 st.sidebar.success("Select a page above.")
+
+#Read in data from Github
+url = 'https://raw.githubusercontent.com/hankshackleford/KorbelFantasyApp/main/owner%20data.csv'
+df_selected = pd.read_csv(url)
+print(df_selected)
+
+#setting palette
+colors=['#1A8A41','#521052']
+
+win_loss = px.bar(
+    data_frame = df_selected,
+    x = "Owner",
+    y = ["Wins","Losses"],
+    orientation = "v",
+    barmode = 'group',
+    color_discrete_sequence=colors,
+    opacity= .8,
+    title='Wins and Losses',
+)
+st.plotly_chart(win_loss)
