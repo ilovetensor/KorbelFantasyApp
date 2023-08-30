beeebd1db928a9bc3af1ddf75eb56f9c034b55b1
Rohan Sharma <96976448+ilovetensor@users.noreply.github.com>
2023-08-30 05:52:59 +0530

    Update dws.py

--- dws.py
+++ dws.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
+st.text("**hdhwv** $hs")
+st.write("**hshevh** $hd")
 
-with st.echo():
-    st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+st.markdown("**hshevh** $hs")
